5e486860a46a52748f61c06e3a98b3f76903717d
SwayKh <khatrisumit712@gmail.com>
2023-11-22 23:13:11 +0530

    Update extension checking logic, add more echo outputs

--- install.sh
+++ install.sh
@@ -12,13 +12,18 @@ else
 fi
 
 # Get the directory where the plugin is installed(System or local)
+echo "Checking if GS-Connect extension if installed"
 if [ -e "$USER_HOME/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/" ]; then
   extension_dir="$USER_HOME/.local/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/"
-else
+elif [ -e "/usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/" ]; then
   extension_dir="/usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/service/"
+else
+  echo "Extension not installed."
+  echo "Please install gsconnect extension"
+  exit 0
 fi
 
-echo ""
+echo "Checking if the patch has been applied"
 block_of_code="this block of code is not part of gsconnect connect extension"
 if grep -q "$block_of_code" "$extension_dir/plugins/sftp.js"; then
   echo "Patch is already applied"
@@ -31,6 +36,7 @@ script_dir=$(dirname "$0")
 cd "$script_dir" || exit # Exit the script if cd doesn't work, prevents following commands from running
 
 # putting files in place
+echo "Updating mountInfoHandler.js"
 cp -f ./mountInfoHandler.js "$extension_dir/utils/"
 mkdir -p "$USER_HOME/.config/gsconnect-mount-manager/" && cp -f ./run.py "$USER_HOME/.config/gsconnect-mount-manager/"
 [ -e /etc/systemd/user/gsconnect-mount-manager.service ] || sudo touch /etc/systemd/user/gsconnect-mount-manager.service
@@ -38,14 +44,11 @@ mkdir -p "$USER_HOME/.config/gsconnect-mount-manager/" && cp -f ./run.py "$USER_
 # update service file
 # Change > to " | sudo tee " because shell doesn't have to permission to redirect to system file
 # ( It would work if the whole script is run with sudo )
-#
 echo "Creating a user systemd service"
 echo " "
 sudo ./update_servicefile.py "$USER_HOME" "$script_dir" | sudo tee /etc/systemd/user/gsconnect-mount-manager.service
 
 # editing sftp.js in gsconnect extension
-#
-
 echo " "
 echo "Editing sftp.js in gsconnect extension"
 sudo ./update_sftp.py "$USER_HOME"
